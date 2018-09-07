ab438c7d98b355b283797dde2d3a6ae6da61511a
tuxxy <john@nucypher.com>
2018-09-07 16:33:53 -0600

    Create project stuffs and setup

---
+++ tests/test_parser.py
@@ -0,0 +1,19 @@
+from sputnik.parser import Parser
+
+
+def test_parser_read_testfile():
+    SputnikParser = Parser('tests/test.sputnik')
+
+    assert SputnikParser.raw_data is not ''
+    assert len(SputnikParser.lines) == 3
+    assert len(SputnikParser.operations) == 3
+
+    # Test Operations lengths
+    bootstrap = SputnikParser.operations[0]
+    assert len(bootstrap) == 3
+
+    xor = SputnikParser.operations[1]
+    assert len(xor) == 5
+
+    end = SputnikParser.operations[2]
+    assert len(end) == 1
